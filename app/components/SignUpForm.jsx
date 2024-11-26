1eae865d624cb9c493d4ec939f0399dac5dc3af8
John A. Samuel <johnayobami77@proton.me>
2024-11-26 17:16:40 +0100

    updates

--- app/components/SignUpForm.jsx
+++ app/components/SignUpForm.jsx
@@ -26,7 +26,7 @@ const App = () => {
 
   return (
     <div className='lg:grid lg:grid-cols-2 bg-gray-800 w-full h-screen overflow-hidden'>
-      <div className="w-full flex flex-col text-white space-y-3 px-10 md:px-16 lg:pl-16 lg:px-0 xl:pl-16 xl:pr-16 py-10 lg:py-8">
+      <div className="w-full flex flex-col text-white space-y-3 px-10 md:px-16 lg:px-10 xl:px-32 py-10 lg:py-8">
         <h1 className='text-4xl font-bold text-center'>Create Account</h1>
         <p className="text-gray-300 text-center">Join the community and unlock your learning potential</p>
 
