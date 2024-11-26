85a44b6e406219b7d23cb26187cb802354a9fba7
John A. Samuel <johnayobami77@proton.me>
2024-11-26 13:58:51 +0100

    Updates

--- app/page.jsx
+++ app/page.jsx
@@ -25,9 +25,10 @@ const App = () => {
   };
 
   return (
-    <div className='lg:grid lg:grid-cols-2 bg-gray-900'>
-      <div className="flex flex-col text-white gap-5 px-16 lg:px-10 xl:px-24 py-12 lg:py-8">
-        <h1 className='text-4xl lg:text-6xl font-bold text-center'>Create Account</h1>
+    <div className='lg:grid lg:grid-cols-2 bg-black w-full h-full overflow-hidden'>
+      <div className="flex flex-col text-white gap-3 px-16 lg:px-10 xl:px-32 py-20 lg:py-8">
+        <h1 className='text-4xl font-bold text-center'>Create Account</h1>
+        <p>Join us </p>
         
 
         <div className='mt-4'>
@@ -44,11 +45,11 @@ const App = () => {
           </div>
         </div>
 
-        <form onSubmit={handleSubmit} className='flex flex-col gap-3 lg:gap-6'>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-3 lg:gap-4'>
             <fieldset className="flex flex-col w-full items-start justify-center gap-1">
               <label className='text-white' htmlFor="firstname">Name</label>
               <input
-                className="w-full border border-solid py-3 px-3 rounded bg-gray-800 border-gray-300 outline-none focus:ring-1 focus:ring-[#ffca0d] focus:border-none transition-all duration-300 ease-in-out text-white"
+                className="w-full border border-solid py-4 px-4 rounded-full bg-gray-800 border-gray-300 outline-none focus:ring-1 focus:ring-primary focus:border-none transition-all duration-300 ease-in-out text-white"
                 type="text"
                 name="name"
                 id="name"
@@ -62,7 +63,7 @@ const App = () => {
           <fieldset className="flex flex-col items-start justify-center gap-1">
             <label className='text-white' htmlFor="email">Email Address</label>
             <input
-              className="w-full border border-solid py-3 px-3 rounded border-white bg-gray-800 outline-none focus:ring-1 focus:ring-primary focus:border-none transition-all duration-300 ease-in-out text-white"
+              className="w-full border border-solid py-4 px-4 rounded-full border-gray-300 bg-gray-800 outline-none focus:ring-1 focus:ring-primary focus:border-none transition-all duration-300 ease-in-out text-white"
               type="email"
               name="email"
               id="email"
@@ -95,18 +96,18 @@ const App = () => {
                 checked={formData.agree}
                 onChange={handleChange}
               />
-              <label htmlFor="agree" className="text-white">I agree to all the <a className="font-bold underlined">terms and conditions</a>.</label>
+              <label htmlFor="agree" className="text-white">I agree to all the <a className="font-bold underline">terms and conditions</a>.</label>
             </div>
           </div>
 
           <button className='mt-6 w-full bg-primary text-white transition duration-300 ease-out py-3 rounded-[36px]' type="submit">
             Sign Up
           </button>
-          <p className="text-white">Already have an account? <Link href="/login" className='font-bold underlined transition duration-300 ease-in-out'>Log in</Link></p>
+          <p className="text-white">Already have an account? <Link href="/login" className='font-bold underline transition duration-300 ease-in-out'>Log in</Link></p>
         </form>
         
       </div>
-      <div className='hidden lg:block lg:w-5/6 xl:w-11/12 relative h-full bg-gradient-to-tr from-cyan-800 to-purple-900'>
+      <div className='hidden lg:block ml-auto lg:w-5/6 xl:w-11/12 relative h-full bg-gradient-to-tr from-cyan-800 to-purple-900'>
         <div className='absolute w-full opacity-40 h-full bg-black backdrop-blur-xl'></div>
         <div className='relative text-white top-1/4 left-5'>
           <div className='flex flex-col items-center justify-between gap-10 px-8'>
