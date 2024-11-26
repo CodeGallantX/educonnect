ad019f65d421def38e9c6e754d99c67fea31182b
John A. Samuel <johnayobami77@proton.me>
2024-11-26 14:34:05 +0100

    Updates

--- app/page.jsx
+++ app/page.jsx
@@ -28,8 +28,8 @@ const App = () => {
     <div className='lg:grid lg:grid-cols-2 bg-gray-800 w-full h-full overflow-hidden'>
       <div className="flex flex-col text-white gap-3 px-16 lg:px-10 xl:px-32 py-20 lg:py-8">
         <h1 className='text-4xl font-bold text-center'>Create Account</h1>
-        <p className="text-gray-300">Join us </p>
-        
+        <p className="text-gray-300">Join the community and unlock your learning potential</p>
+
 
         <div className='mt-4'>
           <div className="flex flex-col items-center justify-center gap-4">
@@ -45,29 +45,29 @@ const App = () => {
           </div>
         </div>
 
-        <form onSubmit={handleSubmit} className='flex flex-col gap-3 lg:gap-4'>
-            <fieldset className="flex flex-col w-full items-start justify-center gap-1">
-              <label className='text-white' htmlFor="firstname">Name</label>
-              <input
-                className="w-full border border-solid py-4 px-4 rounded-full bg-gray-800 border-gray-300 outline-none focus:ring-1 focus:ring-primary focus:border-none transition-all duration-300 ease-in-out text-white"
-                type="text"
-                name="name"
-                id="name"
-                placeholder='Kia Smith'
-                value={formData.name}
-                onChange={handleChange}
-                required
-              />
-            </fieldset>
+        <form onSubmit={handleSubmit} className='flex flex-col space-y-3 lg:space-y-4'>
+          <fieldset className="flex flex-col w-full items-start justify-center gap-1">
+            <label className='text-white' htmlFor="name">Name</label>
+            <input
+              className="w-full border border-solid py-4 px-6 rounded-full bg-gray-800 border-gray-500 outline-none focus:ring-1 focus:ring-gray-300 focus:border-none transition-all duration-300 ease-in-out text-white"
+              type="text"
+              name="name"
+              id="name"
+              placeholder='Enter your name'
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </fieldset>
 
           <fieldset className="flex flex-col items-start justify-center gap-1">
             <label className='text-white' htmlFor="email">Email Address</label>
             <input
-              className="w-full border border-solid py-4 px-4 rounded-full border-gray-300 bg-gray-800 outline-none focus:ring-1 focus:ring-primary focus:border-none transition-all duration-300 ease-in-out text-white"
+              className="w-full border border-solid py-4 px-6 rounded-full border-gray-500 bg-gray-800 outline-none focus:ring-1 focus:ring-gray-300 focus:border-none transition-all duration-300 ease-in-out text-white"
               type="email"
               name="email"
               id="email"
-              placeholder='kiasmith@mail.com'
+              placeholder='Enter your email address'
               value={formData.email}
               onChange={handleChange}
               required
@@ -76,11 +76,11 @@ const App = () => {
           <fieldset className="flex flex-col items-start justify-center gap-1">
             <label className='text-white' htmlFor="password">Password</label>
             <input
-              className="w-full border border-solid py-3 px-3 rounded-full bg-gray-800 border-gray-300 outline-none focus:ring-1 focus:ring-primary focus:border-none transition-all duration-300 ease-in-out text-white"
+              className="w-full border border-solid py-4 px-6 rounded-full bg-gray-800 border-gray-500 outline-none focus:ring-1 focus:ring-gray-300 focus:border-none transition-all duration-300 ease-in-out text-white"
               type="password"
               name="password"
               id="password"
-              placeholder='Enter password'
+              placeholder='**********'
               value={formData.password}
               onChange={handleChange}
               required
@@ -105,19 +105,14 @@ const App = () => {
           </button>
           <p className="text-white">Already have an account? <Link href="/login" className='font-bold underline transition duration-300 ease-in-out'>Log in</Link></p>
         </form>
-        
+
       </div>
-      <div className='hidden lg:block ml-auto lg:w-5/6 xl:w-11/12 relative h-full bg-gradient-to-tr from-cyan-800 to-purple-900'>
-        <div className='absolute w-full opacity-40 h-full bg-black backdrop-blur-xl'></div>
-        <div className='relative text-white top-1/4 left-5'>
-          <div className='flex flex-col items-center justify-between gap-10 px-8'>
-            <Link id="logo" href="/" className="text-white text-3xl font-bold font-main">
-              Civic<span className="text-[#ffca0d]">Track</span>
-            </Link>
-            <div className="flex flex-col gap-2 items-start px-6 ">
-              <blockquote className="text-left text-4xl">&ldquo;The secret of getting ahead is getting started.&rdquo;</blockquote>
-              <em className='text-right text-xl'>— Mark Twain</em>
-            </div>
+      <div className='hidden overflow-hidden lg:block ml-auto lg:w-5/6 xl:w-11/12 relative h-full bg-gradient-to-tr from-cyan-800 to-purple-900'>
+        <div className='absolute bottom-0 w-full opacity-40 h-2/5 bg-black blur'></div>
+        <div className='absolute text-white w-full bottom-1/4 left-1/2 -translate-x-1/2'>
+          <div className="flex flex-col gap-2 items-center px-4">
+            <h1 className="text-4xl text-white text-center">Ask Questions with Ease</h1>
+            <blockquote className="text-left text-2xl">&ldquo;The secret of getting ahead is getting started.&rdquo;</blockquote>
           </div>
         </div>
       </div>
