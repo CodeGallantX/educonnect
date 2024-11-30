7259088c73859b400398cc0c53bdb9ba2fa67775
John A. Samuel <johnayobami77@proton.me>
2024-11-30 14:40:37 +0100

    Updates

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -7,18 +7,18 @@ import { IoGridOutline } from "react-icons/io5";
 
 const Header = () => {
     return (
-        <header className="flex flex-wrap items-center justify-between space-x-10 py-4 px-4 md:px-6 lg:px-16 xl:px-24 bg-white bg-gray-900">
+        <header className="flex flex-wrap items-center justify-between space-x-10 py-4 px-4 md:px-6 lg:px-16 xl:px-24 bg-gray-900">
             <div className="flex-shrink-0 w-32 md:w-52">
                 <img src="/logo.png" alt="Logo" className="w-full h-auto object-contain" />
             </div>
             <nav className="hidden sm:flex space-x-5 ml-auto">
-                <Link href="/home" className="text-gray-400 hover:text-gray-600 dark:hover:text-white">
+                <Link href="/home" className="text-gray-400 hover:text-gray-600 hover:text-white">
                     <PiHouseBold className="text-2xl" />
                 </Link>
-                <Link href="/post" className="text-gray-400 hover:text-gray-600 dark:hover:text-white">
+                <Link href="/post" className="text-gray-400 hover:text-gray-600 hover:text-white">
                     <PiNotePencilBold className="text-2xl" />
                 </Link>
-                <Link href="/community" className="text-gray-400 hover:text-gray-600 dark:hover:text-white">
+                <Link href="/community" className="text-gray-400 hover:text-gray-600 hover:text-white">
                     <IoGridOutline className="text-2xl" />
                 </Link>
             </nav>
@@ -26,7 +26,7 @@ const Header = () => {
                 <input
                     type="search"
                     id="searchBox"
-                    className="w-full max-w-lg rounded-md py-2 px-4 text-gray-600 dark:text-white text-sm bg-gray-200 dark:bg-gray-800 border border-gray-300 dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-primary"
+                    className="w-full max-w-lg rounded-md py-2 px-4 text-gray-600 text-white text-sm bg-gray-200 bg-gray-800 border border-gray-300 border-gray-600 focus:outline-none focus:ring-2 focus:ring-primary"
                     placeholder="Search..."
                 />
             </div>
@@ -37,11 +37,11 @@ const Header = () => {
                     className="w-8 h-8 md:w-10 md:h-10 rounded-full object-cover"
                 />
                 <Link href="#" className='relative w-full'>
-                    <FaRegBell className="text-gray-400 hover:text-gray-600 dark:hover:text-white text-xl md:text-2xl" />
+                    <FaRegBell className="text-gray-400 hover:text-gray-600 hover:text-white text-xl md:text-2xl" />
                     <span className="absolute text-[11px] -top-2 -right-2 rounded-full py-0.5 px-2 bg-red-500 text-white">3</span>
                 </Link>
                 <Link href="/community">
-                    <PiGlobeBold className="text-gray-400 hover:text-gray-600 dark:hover:text-white text-xl md:text-2xl" />
+                    <PiGlobeBold className="text-gray-400 hover:text-gray-600 hover:text-white text-xl md:text-2xl" />
                 </Link>
             </div>
             <Link href="/edit-profile">
